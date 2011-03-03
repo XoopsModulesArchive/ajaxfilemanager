f3bab9111fccdaa6ad0801297bcb5fd98c8aed45
luciorota <luciorota@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-03-03 15:17:09 +0000

    +improved extentions manager

--- XoopsModules/ajaxfilemanager/trunk/ajaxfilemanager/language/english/admin.php
+++ XoopsModules/ajaxfilemanager/trunk/ajaxfilemanager/language/english/admin.php
@@ -16,58 +16,55 @@ define('_AJAXFM_AM_DIRNOTCREATED', "<strong style='color:red;'>WARNING: %s direc
 //define('_AJAXFM_AM_PERM_UPLOAD',"Upload Files");
 //define('_AJAXFM_AM_PERM_UPLOAD_DESC',"IN PROGRESS");
 define('_AJAXFM_AM_PERM_EXTRA',"Module Permissions");
-define('_AJAXFM_AM_PERM_EXTRA_DESC',"Webmasters CAN ALWAYS DO EVERYTHING");
-define('_AJAXFM_AM_PERM_EXTRA_1',"<b>Use Ajax File Manager</b>");
-define('_AJAXFM_AM_PERM_EXTRA_2',"Upload Files");
-define('_AJAXFM_AM_PERM_EXTRA_4',"Delete Files");
-define('_AJAXFM_AM_PERM_EXTRA_8',"Cut Files");
-define('_AJAXFM_AM_PERM_EXTRA_16',"Copy Files");
-define('_AJAXFM_AM_PERM_EXTRA_32',"Create New Folder");
-define('_AJAXFM_AM_PERM_EXTRA_64',"Rename");
-define('_AJAXFM_AM_PERM_EXTRA_128',"Image editor and Text editor");
+define('_AJAXFM_AM_PERM_EXTRA_DESC', "Webmasters CAN ALWAYS DO EVERYTHING");
+define('_AJAXFM_AM_PERM_EXTRA_1', "<b>Use Ajax File Manager</b>");
+define('_AJAXFM_AM_PERM_EXTRA_2', "Upload Files");
+define('_AJAXFM_AM_PERM_EXTRA_4', "Delete Files");
+define('_AJAXFM_AM_PERM_EXTRA_8', "Cut Files");
+define('_AJAXFM_AM_PERM_EXTRA_16', "Copy Files");
+define('_AJAXFM_AM_PERM_EXTRA_32', "Create New Folder");
+define('_AJAXFM_AM_PERM_EXTRA_64', "Rename");
+define('_AJAXFM_AM_PERM_EXTRA_128', "Image editor and Text editor");
 
 
 
 // admin/extentions.php
-define('_AJAXFM_AM_EXTENTION',"Extention");
-define('_AJAXFM_AM_EXTENTION_STATUS',"Status");
-define('_AJAXFM_AM_EXTENTION_ACTION',"Action");
-define('_AJAXFM_AM_EXTENTION_INFO',"Extention information");
-define('_AJAXFM_AM_EXTENTION_NOT_INSTALLED',"Extention not installed");
-define('_AJAXFM_AM_EXTENTION_NOT_ACTIVATED',"Extention not activated");
-define('_AJAXFM_AM_INSTALL_EXTENTION',"Install extention");
-define('_AJAXFM_AM_ACTIVATE_EXTENTION',"Activate extention");
-define('_AJAXFM_AM_EXTENTION_ACTIVATED',"Extention activated");
-define('_AJAXFM_AM_DISABLE_EXTENTION',"Disable extention");
-define('_AJAXFM_AM_EXTENTION_DISABLED',"Extention disabled");
-define('_AJAXFM_AM_EXTENTION_INSTALLED_OK',"Extention installed");
-define('_AJAXFM_AM_EXT_FILE_NOT_INSTALLABLE',"Extention not installable");
-define('_AJAXFM_AM_EXTENTION_ACTIVATED_OK',"Extention activated");
-define('_AJAXFM_AM_EXTENTION_NOTICE',"This extention allow you to display images on all the site just by adding a <b>video</b> tag to your text. A button (<img src=\"../images/image_button.png\" />) is displayed on XOOPS editor.");
-define('_AJAXFM_AM_EXT_FILE_DONT_EXIST',"Extention file don't exist on repository :<br /><b>Server : </b>%s<br /><b>File : </b>%s");
-define('_AJAXFM_AM_EXT_FILE_DONT_EXIST_SHORT',"Extention file don't exist");
-define('_AJAXFM_AM_EXTENTION_INSTALLED',"Extention installed");
+define('_AJAXFM_AM_EXTENTION_WARNING1', "<span style='color:red;'>WARNING: Only if &quot;%s&quot; directory and &quot;config.php&quot; file in it are writable is possible to install, activate and disable extentions using this tool</span>");
+define('_AJAXFM_AM_EXTENTION_WARNING2', "<span style='color:red;'>In any other case you have to copy extentions in &quot;%s&quot; directory and edit &quot;config.php&quot; manually</span>");
+
+define('_AJAXFM_AM_EXTENTION', "Extention");
+define('_AJAXFM_AM_EXTENTION_STATUS', "Status");
+define('_AJAXFM_AM_EXTENTION_ACTION', "Action");
+define('_AJAXFM_AM_EXTENTION_INFO', "Default extentions information");
+define('_AJAXFM_AM_EXTENTION_INFO_DESC', "");
+define('_AJAXFM_AM_EXTRA_EXTENTION_INFO', "Extra extentions information");
+define('_AJAXFM_AM_EXTRA_EXTENTION_INFO_DESC', "");
+define('_AJAXFM_AM_EXTENTION_NOT_INSTALLED', "Extention not installed");
+define('_AJAXFM_AM_EXTENTION_NOT_ACTIVATED', "Extention not activated");
+define('_AJAXFM_AM_INSTALL_EXTENTION', "Install extention");
+define('_AJAXFM_AM_ACTIVATE_EXTENTION', "Activate extention");
+define('_AJAXFM_AM_EXTENTION_ACTIVATED', "Extention activated");
+define('_AJAXFM_AM_DISABLE_EXTENTION', "Disable extention");
+define('_AJAXFM_AM_EXTENTION_DISABLED', "Extention disabled");
+define('_AJAXFM_AM_EXTENTION_INSTALLED_OK', "Extention installed");
+define('_AJAXFM_AM_EXT_FILE_NOT_INSTALLABLE', "Extention not installable");
+define('_AJAXFM_AM_EXTENTION_ACTIVATED_OK', "Extention activated");
+define('_AJAXFM_AM_EXTENTION_NOTICE', "This extention allow you to display images on all the site just by adding a <b>video</b> tag to your text. A button (<img src=\"../images/image_button.png\" />) is displayed on XOOPS editor.");
+define('_AJAXFM_AM_EXT_FILE_DONT_EXIST', "Extention file don't exist on repository :<br /><b>Server : </b>%s<br /><b>File : </b>%s");
+define('_AJAXFM_AM_EXT_FILE_DONT_EXIST_SHORT', "Extention file don't exist");
+define('_AJAXFM_AM_EXTENTION_INSTALLED', "Extention installed");
 
 
 
 //admin/about.php
-define("_AJAXFM_AM_ABOUT_AUTHOR","Author");
-define("_AJAXFM_AM_ABOUT_CHANGELOG","Change log");
-define("_AJAXFM_AM_ABOUT_CREDITS","Credits");
-define("_AJAXFM_AM_ABOUT_LICENSE","License");
-define("_AJAXFM_AM_ABOUT_MODULEINFOS","Module Informations");
-define("_AJAXFM_AM_ABOUT_MODULEWEBSITE","Support Website");
-define("_AJAXFM_AM_ABOUT_RELEASEDATE","Date of launch");
-define("_AJAXFM_AM_ABOUT_STATUS","Status");
-define("_AJAXFM_AM_ABOUT_DESCRIPTION","Module Description &quot;description.html&quot;");
-define("_AJAXFM_AM_ABOUT_HELP","Module Help &quot;help.html&quot;");
-
-
-
-define('_AJAXFM_AM_OPTIONS_MANAGER', 'Options manager IN PROGRESS');
-//define('_AJAXFM_AM_ADDOPTION', 'Add new option');
-//define('_AJAXFM_AM_OPTIONNAME', 'Option name');
-//define('_AJAXFM_AM_OPTIONVALUE', 'Option value');
-//define('_AJAXFM_AM_OPTIONDESCRIPTION', 'Description');
-
+define("_AJAXFM_AM_ABOUT_AUTHOR", "Author");
+define("_AJAXFM_AM_ABOUT_CHANGELOG", "Change log");
+define("_AJAXFM_AM_ABOUT_CREDITS", "Credits");
+define("_AJAXFM_AM_ABOUT_LICENSE", "License");
+define("_AJAXFM_AM_ABOUT_MODULEINFOS", "Module Informations");
+define("_AJAXFM_AM_ABOUT_MODULEWEBSITE", "Support Website");
+define("_AJAXFM_AM_ABOUT_RELEASEDATE", "Date of launch");
+define("_AJAXFM_AM_ABOUT_STATUS", "Status");
+define("_AJAXFM_AM_ABOUT_DESCRIPTION", "Module Description &quot;description.html&quot;");
+define("_AJAXFM_AM_ABOUT_HELP", "Module Help &quot;help.html&quot;");
 ?>
