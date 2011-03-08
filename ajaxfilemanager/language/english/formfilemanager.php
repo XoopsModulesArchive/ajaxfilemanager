a60ab3be7f85af7de0b992b8f6d80b7c4e5291b1
luciorota <luciorota@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-03-08 21:20:49 +0000

    deleted unused files

--- ajaxfilemanager/language/english/formfilemanager.php
+++
@@ -1,5 +0,0 @@
-<?php
-//class/formfilemanager.php
-define("_FORMFILEMANAGER_FILEMANAGER", "Open File Manager");
-define("_FORMFILEMANAGER_FILEURL", "File url:");
-?>
--- ajaxfilemanager/language/english/formkmleditor.php
+++
@@ -1,5 +0,0 @@
-<?php
-//class/formkmleditor.php
-define("_FORMKLMEDITOR_KMLCODE", "Kml Code");
-define("_FORMKMLEDITOR_KMLEDITOR", "Kml Editor");
-?>
